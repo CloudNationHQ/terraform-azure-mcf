73adc0938a478973ef4e47bfced87ffa31b9dea0
Dennis Kool <43271994+dkooll@users.noreply.github.com>
2026-04-03 09:28:02 +0200

    feat: add missing outputs (#18)

--- outputs.tf
+++ outputs.tf
@@ -2,3 +2,13 @@ output "config" {
   description = "Contains all maintenance configuration"
   value       = azurerm_maintenance_configuration.mcf
 }
+
+output "vm_assignments" {
+  description = "Contains all virtual machine assignments"
+  value       = azurerm_maintenance_assignment_virtual_machine.mcf_vm
+}
+
+output "dynamic_scope_assignments" {
+  description = "Contains all dynamic scope assignments"
+  value       = azurerm_maintenance_assignment_dynamic_scope.mcf_ds
+}
